30939713ca1849aca7be74244868bb2637ca8fcf
kaliniArt <kalini.art@gmail.com>
2020-11-08 05:58:37 +0200

    change header.php

--- wp-content/themes/woodmart/header.php
+++ wp-content/themes/woodmart/header.php
@@ -4,8 +4,7 @@ ini_set('zlib.output_compression_level', '3'); ?>
 /**
  * The Header template for our theme
  */
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="ru-RU">
 <head>
 	<meta charset="UTF-8">
